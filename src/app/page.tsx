681d712c2c2895f539079cfbe49d3c7819bc7246
Christian Toribio <chris09toribio@gmail.com>
2025-03-17 15:13:18 -0700

    force dynamic test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { ProductTable } from "@/src/components/product_table/product-table";
 import { Product } from "../db/schema";
 import { getProducts } from "../db/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   let products: Product[] = await getProducts();
 
